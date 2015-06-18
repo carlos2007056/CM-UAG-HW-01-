36d1ddbf55f300d1277b130abb7d9614456e725f
bennsandoval <bennsandoval@gamil.com>
2015-06-18 11:24:50 -0500

    Initial Commit

---
+++ master.mobile/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  master.mobile
+//
+//  Created by Macbook Pro on 6/18/15.
+//  Copyright (c) 2015 Benn Sandoval. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
